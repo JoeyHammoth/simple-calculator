5fa84764df18b63dad0e0443a56441a64a783d59
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-01-06 22:31:29 +0100

    Finished creating simple calculator

--- Calculator/src/main/java/base/Calculation.java
+++ Calculator/src/main/java/base/Calculation.java
@@ -28,52 +28,113 @@ public class Calculation {
         HBox first = new HBox();
         VBox.setMargin(first, new Insets(10.0d));
         Button b1 = new Button("1");
-        b1.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "1");});
+        b1.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "1");});
         Button b2 = new Button("2");
-        b2.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "2");});
+        b2.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "2");});
         Button b3 = new Button("3");
-        b3.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "3");});
+        b3.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "3");});
         Button bPlus = new Button("+");
-        bPlus.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "+");});
+        bPlus.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + " + ");});
         Button bMinus = new Button("-");
-        bMinus.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "-");});
+        bMinus.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + " - ");});
         first.getChildren().addAll(b1, b2, b3, bPlus, bMinus);
 
         HBox second = new HBox();
         VBox.setMargin(second, new Insets(10.0d));
         Button b4 = new Button("4");
-        b4.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "4");});
+        b4.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "4");});
         Button b5 = new Button("5");
-        b5.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "5");});
+        b5.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "5");});
         Button b6 = new Button("6");
-        b6.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "6");});
+        b6.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "6");});
         Button bProduct = new Button("×");
-        bProduct.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "×");});
+        bProduct.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + " × ");});
         Button bQuotient = new Button("÷");
-        bQuotient.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "÷");});
+        bQuotient.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + " ÷ ");});
         second.getChildren().addAll(b4, b5, b6, bProduct, bQuotient);
 
         HBox third = new HBox();
-        VBox.setMargin(second, new Insets(10.0d));
+        VBox.setMargin(third, new Insets(10.0d));
         Button b7 = new Button("7");
-        b7.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "7");});
+        b7.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "7");});
         Button b8 = new Button("8");
-        b8.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "8");});
+        b8.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "8");});
         Button b9 = new Button("9");
-        b9.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "9");});
+        b9.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "9");});
         Button b0 = new Button("0");
-        b0.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + "0");});
+        b0.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + "0");});
         Button bDecimal = new Button(".");
-        bDecimal.setOnAction(actionEvent -> {displayContent.set(displayContent.toString() + ".");});
+        bDecimal.setOnAction(actionEvent -> {displayContent.set(displayContent.get() + ".");});
         third.getChildren().addAll(b7, b8, b9, b0, bDecimal);
 
         Button bEquals = new Button("=");
-        HBox fourth = new HBox(bEquals);
-        fourth.setAlignment(Pos.CENTER);
+        bEquals.setId("Equal");
+        bEquals.setOnAction(actionEvent -> {generateSol(displayContent);});
+
+        Button bClear = new Button("Clear");
+        bClear.setId("Clear");
+        bClear.setOnAction(actionEvent -> {displayContent.set("");});
+
+        HBox fourth = new HBox();
+        fourth.getChildren().addAll(bEquals, bClear);
+        VBox.setMargin(fourth, new Insets(10.0d));
 
         vbox.getChildren().addAll(zero, first, second, third, fourth);
 
         Scene scene = new Scene(vbox);
         return scene;
+    }
+
+    public static void generateSol(StringProperty displayContent) {
+        boolean error = false;
+        String[] elements = displayContent.get().split(" ");
+        char op = '0';
+        double result = 0;
+        for (String str : elements) {
+            if (!str.equals("+") && !str.equals("-") && !str.equals("×") && !str.equals("÷")) {
+                if (op == '0' || op == '+') {
+                    try {
+                        result += Double.parseDouble(str);
+                    } catch (Exception e) {
+                        error = true;
+                        break;
+                    }
+                } else if (op == '-') {
+                    try {
+                        result -= Double.parseDouble(str);
+                    } catch (Exception e) {
+                        error = true;
+                        break;
+                    }
+                } else if (op == '×') {
+                    try {
+                        result *= Double.parseDouble(str);
+                    } catch (Exception e) {
+                        error = true;
+                        break;
+                    }
+                } else if (op == '÷') {
+                    try {
+                        result /= Double.parseDouble(str);
+                    } catch (Exception e) {
+                        error = true;
+                        break;
+                    }
+                }
+            } else {
+                op = switch (str) {
+                    case "+" -> '+';
+                    case "-" -> '-';
+                    case "×" -> '×';
+                    case "÷" -> '÷';
+                    default -> op;
+                };
+            }
+        }
+        if (error) {
+            displayContent.set("Error! Please clear and try again!");
+        } else {
+            displayContent.set(String.valueOf(result));
+        }
     }
 }
--- Calculator/src/main/java/base/Main.java
+++ Calculator/src/main/java/base/Main.java
@@ -1,11 +1,17 @@
 package base;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public void start(Stage stage) throws Exception {
-
+        Scene scene = Calculation.createCalcScene();
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.show();
     }
 
     public static void main(String[] args) {
